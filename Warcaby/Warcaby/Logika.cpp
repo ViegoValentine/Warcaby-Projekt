d68ad3fcd02093e460a2fc12437576d274892a17
olcim04 <135529833+olcim04@users.noreply.github.com>
2023-06-11 12:52:57 +0200

    Update Logika.cpp

--- Warcaby/Warcaby/Logika.cpp
+++ Warcaby/Warcaby/Logika.cpp
@@ -1,7 +1,43 @@
 #include "Logika.h"
 
-void Logika::mozliwosciRuchu(Pionek pionki[], zmiennaX, zmiennaY)
+void Logika::mozliwosciRuchu(Pionek pionki[], zmiennaX, zmiennaY, znak)
 {
+  char doPrzesuniecia;
+  char zastepczyX;
+  int zastepczyY;
+ 
+  //sprawdzanie mozliwosciu ruchu dla czlowieka
+  //biale pionki
+  if(znak=="O")
+  {
+    cout<<"Podaj ktory pionek przesunać";
+    cin>>doPrzesuniecia;
+    cout<<endl;
+    cout<<"Podaj pole mna ktore go przesunac, zmienna X:";
+    cin >> zmiannaX;
+    cout<<"podaj zmienna Y: ";
+    cin>>zmiennaY;
+    
+    zastepczyX=zmiennaX; //zeby nie dzialac na podawanych zmiennych
+    zastepczyY=zmiennaY;
+    
+    
+  }
+  
+  //czarne pionki
+  if (znak=="X")
+  {
+    cout<<"Podaj ktory pionek przesunać";
+    cin>>doPrzesuniecia;
+    cout<<endl;
+    cout<<"Podaj pole mna ktore go przesunac, zmienna X:";
+    cin >> zmiannaX;
+    cout<<"podaj zmienna Y: ";
+    cin>>zmiennaY;
+    
+    zastepczyX=zmiennaX; //zeby nie dzialac na podawanych zmiennych
+    zastepczyY=zmiennaY;
+  }
   
 }
 
